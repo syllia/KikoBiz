75fae930a306abe30f19b816a6f88bb5da01ebd9
ianel20 <j.adossou@live.fr>
2016-05-20 01:12:50 -0400

    test.bin

--- KikoBiz/src/bj/kiko/projects/kikobiz/Rest/SousCategorieController.java
+++ KikoBiz/src/bj/kiko/projects/kikobiz/Rest/SousCategorieController.java
@@ -10,45 +10,38 @@ import com.googlecode.objectify.ObjectifyService;
 import bj.kiko.projects.kikobiz.Services.SousCategorieService;
 import bj.kiko.projects.kikobiz.model.SousCategorie;
 import bj.kiko.projects.kikobiz.model.app.SousCategorieApp;
+
 @Api(name = "monapi")
 public class SousCategorieController {
 	static {
-		 ObjectifyService.register(SousCategorie.class);
+		ObjectifyService.register(SousCategorie.class);
 	}
-		@ApiMethod(
-			path = "souscategories/{idCategorie}",
-			httpMethod = ApiMethod.HttpMethod.GET)
-		public List <SousCategorie>SouscategoriesList(@Named("idCategorie") String iD) {
-			System.out.println(iD);
-			// Récupère toute les Souscategories d'une categorie...
-			List<SousCategorie> souscategories = SousCategorieService.findAllByCategorie(iD);
-			return souscategories ;
-		}
-		@ApiMethod(
-			name = "souscategorie.insert",
-			path = "souscategories",
-			httpMethod = ApiMethod.HttpMethod.POST		        
-			)
-		public SousCategorie save(SousCategorieApp sousCategorie) {
-			System.out.println(sousCategorie.getCategorie());
-			return SousCategorieService.save(sousCategorie);
-		}
-		@ApiMethod(
-		    path = "souscategories/{idCategorie}/{idSousCategorie}/{newName}",
-		    httpMethod = ApiMethod.HttpMethod.PUT
-		    )
-		public SousCategorie modifyID(@Named("idCategorie") String idCategorie, @Named("idSousCategorie") long idSousCategorie,
-				@Named("newName") String newName) {
-		    
-		    return SousCategorieService.modifyById(idCategorie, idSousCategorie, newName);
-		}
-		@ApiMethod(
-			    path = "souscategories/{idCategorie}/{idSousCategorie}/",
-			    httpMethod = ApiMethod.HttpMethod.DELETE
-			    )
-			public SousCategorie deleteById(@Named("idCategorie") String idCategorie,
-					@Named("idSousCategorie") Long idSousCategorie) {
-			    
-			    return SousCategorieService.deleteById(idCategorie, idSousCategorie);
-			}
+
+	@ApiMethod(path = "souscategories/{idCategorie}", httpMethod = ApiMethod.HttpMethod.GET)
+	public List<SousCategorie> SouscategoriesList(@Named("idCategorie") String iD) {
+		System.out.println(iD);
+		// Récupère toute les Souscategories d'une categorie...
+		List<SousCategorie> souscategories = SousCategorieService.findAllByCategorie(iD);
+		return souscategories;
+	}
+
+	@ApiMethod(name = "souscategorie.insert", path = "souscategories", httpMethod = ApiMethod.HttpMethod.POST)
+	public SousCategorie save(SousCategorieApp sousCategorie) {
+		System.out.println(sousCategorie.getCategorie());
+		return SousCategorieService.save(sousCategorie);
+	}
+
+	@ApiMethod(path = "souscategories/{idCategorie}/{idSousCategorie}/{newName}", httpMethod = ApiMethod.HttpMethod.PUT)
+	public SousCategorie modifyID(@Named("idCategorie") String idCategorie,
+			@Named("idSousCategorie") long idSousCategorie, @Named("newName") String newName) {
+
+		return SousCategorieService.modifyById(idCategorie, idSousCategorie, newName);
+	}
+
+	@ApiMethod(path = "souscategories/{idCategorie}/{idSousCategorie}/", httpMethod = ApiMethod.HttpMethod.DELETE)
+	public SousCategorie deleteById(@Named("idCategorie") String idCategorie,
+			@Named("idSousCategorie") Long idSousCategorie) {
+
+		return SousCategorieService.deleteById(idCategorie, idSousCategorie);
+	}
 }
