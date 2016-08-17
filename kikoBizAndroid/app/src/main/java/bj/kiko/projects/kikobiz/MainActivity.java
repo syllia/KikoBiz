55725fa7b595def8113a3b38de4d97a5d3f07f9b
ianel20 <j.adossou@live.fr>
2016-08-16 23:10:09 -0400

    menu

--- kikoBizAndroid/app/src/main/java/bj/kiko/projects/kikobiz/MainActivity.java
+++ kikoBizAndroid/app/src/main/java/bj/kiko/projects/kikobiz/MainActivity.java
@@ -121,17 +121,20 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_post) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_favoris) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_recherche) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_parametre) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_probleme) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_mail) {
+
+        }
+        else if (id == R.id.nav_home) {
 
         }
 
