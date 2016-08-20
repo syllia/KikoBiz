79f91cf83a2e13055659258f6fe4e21be1337ffc
ianel20 <j.adossou@live.fr>
2016-08-20 16:58:45 -0400

    splashscreen

--- kikoBizAndroid/app/src/main/java/bj/kiko/projects/kikobiz/SplashScreenActivity.java
+++ kikoBizAndroid/app/src/main/java/bj/kiko/projects/kikobiz/SplashScreenActivity.java
@@ -1,6 +1,11 @@
 package bj.kiko.projects.kikobiz;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
@@ -11,12 +16,21 @@ import android.view.View;
 
 public class SplashScreenActivity extends AppCompatActivity {
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        if(isNetworkAvailable()){
+            splash();
+        }
+        else{
+            showAlertDialog(SplashScreenActivity.this, "Internet connexion",
+                    "Aucune connexion internet", false);
+        }
+    }
+    public void splash() {
         new Handler().postDelayed(new Runnable() {
             /*
              * Showing splash screen with a timer. This will be useful when you
@@ -28,11 +42,60 @@ public class SplashScreenActivity extends AppCompatActivity {
                 // Start your app main activity
                 Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
                 startActivity(i);
-
                 // close this activity
                 finish();
             }
         }, SPLASH_TIME_OUT);
-    }
 
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null;
+    }
+    public void showAlertDialog(Context context, String title, String message, Boolean status) {
+        AlertDialog.Builder builder =new AlertDialog.Builder(this);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setCancelable(false);
+
+        // Setting Dialog Title
+        alertDialog.setTitle(title);
+
+        // Setting Dialog Message
+        alertDialog.setMessage(message);
+
+        // Setting alert dialog icon
+        // alertDialog.setIcon((status) ? R.mipmap.ic_launcher : R.mipmap.ic_launcher);
+
+        // Setting OK Button
+        alertDialog.setButton("Recommencer", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                new Handler().postDelayed(new Runnable() {
+                    /*
+                     * Showing splash screen with a timer. This will be useful when you
+                     * want to show case your app logo / company
+                     */
+                    @Override
+                    public void run() {
+                        if(isNetworkAvailable()){
+                            splash();
+                        }
+                        else{
+                            showAlertDialog(SplashScreenActivity.this, "Internet connexion",
+                                    "Aucune connexion internet", false);
+                        }
+                    }
+                }, 500);
+
+            }
+        });
+
+        // Showing Alert Message
+        alertDialog.show();
+    }
+    @Override
+    public void onBackPressed(){
+        finish();
+    }
 }
