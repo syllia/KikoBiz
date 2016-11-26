49627f405095d57ead0f00e7b9ba5a84f2615891
Syllia Mêhou-Loko <sylliamehou-loko@MacBook-Pro-de-Syllia.local>
2016-11-26 16:45:03 -0500

    create cuori project

---
+++ kouri/OfferServer/src/main/java/com/kouri/OfferServer/App.java
@@ -0,0 +1,13 @@
+package com.kouri.OfferServer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
