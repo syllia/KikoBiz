a47b0d880321de74a7efe07d852f960b63efe356
Syllia Mêhou-Loko <sylliamehou-loko@MacBook-Pro-de-Syllia.local>
2016-11-26 16:58:50 -0500

    offerServer Sources added

---
+++ kouri/OfferServer/src/main/java/com/kouri/OfferServer/services/SubCategoryInputDTO.java
@@ -0,0 +1,12 @@
+package com.kouri.OfferServer.services;
+
+public class SubCategoryInputDTO {
+	public String subCategoryName;
+	public String categoryName;
+
+	public SubCategoryInputDTO(String subCategoryName, String categoryName) {
+		this.subCategoryName = subCategoryName;
+		this.categoryName = categoryName;
+	}
+
+}
