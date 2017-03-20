1598aefaa885aa4093f6279a14f3b685bc6b7216
ianel20 <j.adossou@live.fr>
2017-03-20 18:30:30 -0400

    image

--- offer-service/src/main/java/com/kouri/ressources/PhotoRessources.java
+++ offer-service/src/main/java/com/kouri/ressources/PhotoRessources.java
@@ -1,25 +1,40 @@
 package com.kouri.ressources;
 
-import java.io.File;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import javax.imageio.ImageIO;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PhotoRessources {
 
-	@RequestMapping(value = "/image", method = RequestMethod.GET, produces = { "image/png", "image/jpeg",
-			"image/gif" }, headers = { "content-type=image/jpeg" })
-	public ResponseEntity<?> findAll() {
-		File file = new File(
-				"/Users/julioadossou/Documents/KikoBiz/offer-service/src/main/resources/14691960_10206121873320366_6430347671553571458_o.jpg");
+	@RequestMapping(value = "/image", method = RequestMethod.GET, produces = "image/jpg")
+	public @ResponseBody byte[] getFile() {
+		try {
+			// Retrieve image from the classpath.
+			InputStream is = new FileInputStream(
+					"/Users/julioadossou/Documents/KikoBiz/offer-service/src/main/resources/14691960_10206121873320366_6430347671553571458_o.jpg");
 
-		return new ResponseEntity<>((Object) file, HttpStatus.OK);
-		// responseBuilder.header("Content-Disposition", "attachment;
-		// filename=\"MyPngImageFile.png\"");
-		// return responseBuilder.build();
+			// Prepare buffered image.
+			BufferedImage img = ImageIO.read(is);
+
+			// Create a byte array output stream.
+			ByteArrayOutputStream bao = new ByteArrayOutputStream();
+
+			// Write to output stream
+			ImageIO.write(img, "jpg", bao);
+
+			return bao.toByteArray();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
